0a924ea99749c78c338c2ee5b5f6a8b6aa9113e5
root <rjmartins@lnec.pt>
2020-09-09 11:16:06 +0000

    added small details to the bash script

--- worsica_jenkins_doc_build_commit.sh
+++ worsica_jenkins_doc_build_commit.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 WORSICADOC_WORKDIR="/usr/local/WORSICA.github.io"
-
+echo "1- Build the documentation from development branch"
+git checkout development
 sphinx-multiversion -c ${WORSICADOC_WORKDIR}/source ${WORSICADOC_WORKDIR}/source ${WORSICADOC_WORKDIR}/html
 cp ${WORSICADOC_WORKDIR}/.git_credential_helper.sh ${WORSICADOC_WORKDIR}/html/
+echo "2- Change to gh-pages and copy outputs to the root folder"
 git checkout gh-pages
 cp -r ${WORSICADOC_WORKDIR}/html/* ${WORSICADOC_WORKDIR}/
 rm -rf ${WORSICADOC_WORKDIR}/html
+echo "3- Commit changes to gh-pages"
 echo ".git_credential_helper.sh" > ${WORSICADOC_WORKDIR}/.gitignore
 git -C ${WORSICADOC_WORKDIR} add -A
 git -C ${WORSICADOC_WORKDIR} reset -- .git_credential_helper.sh
 git config --global user.email 'rjmartins@lnec.pt'
 git commit -m 'Added HTML docs'
 env GIT_ASKPASS=${WORSICADOC_WORKDIR}/.git_credential_helper.sh git -C ${WORSICADOC_WORKDIR} push -u origin gh-pages
+echo "Success!"
